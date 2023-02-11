e171710bb22e67d3d32311f3062bae0ca36fa83a
Arief Nur Misuari <arief.nur@efishery.com>
2023-02-12 04:21:05 +0700

    feat(OledHandler) adding Oled Class

--- src/OledHandler.hpp
+++ src/OledHandler.hpp
@@ -11,15 +11,19 @@
 #define SCREEN_WIDTH 128 // Lebar Tampilan Oled, dalam satuan pixels
 #define SCREEN_HEIGHT 64 // Tinggi Tampilan Oled, dalam satuan pixels
 
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RST);
+// Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RST);
+Adafruit_SSD1306 *display = new Adafruit_SSD1306(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RST);
 
 class OledHandler
 {
 public:
-    bool begin(Adafruit_SSD1306 *);
+    bool begin();
+    void clear();
+    void setCursor(int8_t, int8_t);
+    void print_text(uint16_t, uint8_t, const char *);
 };
 
-bool OledHandler::begin(Adafruit_SSD1306 *display)
+bool OledHandler::begin()
 {
     Wire.begin(OLED_SDA, OLED_SCL);
     if (!display->begin(SSD1306_SWITCHCAPVCC, 0x3c, false, false))
@@ -30,4 +34,18 @@ bool OledHandler::begin(Adafruit_SSD1306 *display)
     }
 
     return true;
+}
+
+void OledHandler::clear() { display->clearDisplay(); }
+
+void OledHandler::setCursor(int8_t x, int8_t y) { 
+    display->setCursor(x, y); 
+}
+
+void OledHandler::print_text(uint16_t color, uint8_t size, const char *text)
+{
+    display->setTextColor(color);
+    display->setTextSize(size);
+    display->print(text);
+    display->display();
 }
